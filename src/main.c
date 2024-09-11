93ada5ab4ab41b36a7a0556f2960db3941115650
Jean-Pascal Richir <jrichir@student.s19.be>
2024-09-11 11:13:43 +0200

    add main() function (still empty)

--- src/main.c
+++ src/main.c
@@ -6,7 +6,12 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 11:11:01 by jrichir           #+#    #+#             */
-/*   Updated: 2024/09/11 11:11:07 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/09/11 11:13:07 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	main(int argc, char **argv)
+{
+	// code
+	return (0);
+}
