d3e02909f505e3d43b1bb5f9e774920aef73a828
Jean-Pascal Richir <jrichir@student.s19.be>
2024-09-11 11:15:41 +0200

    void argc and argv to allow compiling

--- src/main.c
+++ src/main.c
@@ -6,12 +6,16 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 11:11:01 by jrichir           #+#    #+#             */
-/*   Updated: 2024/09/11 11:13:07 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/09/11 11:14:55 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	main(int argc, char **argv)
 {
-	// code
+	/* TEMP */
+	(void)argc;
+	(void)argv;
+	printf("Hello world!\n");
+	/* END TEMP */
 	return (0);
 }
