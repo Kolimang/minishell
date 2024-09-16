79df3875a1963dde7178423152b894ff2f3067b7
Jean-Pascal Richir <jrichir@student.s19.be>
2024-09-16 14:08:03 +0200

    include stdlib.h for free() and unistd.h

--- include/minishell.h
+++ include/minishell.h
@@ -6,14 +6,16 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 11:16:57 by jrichir           #+#    #+#             */
-/*   Updated: 2024/09/13 16:17:23 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/09/16 14:07:27 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <stdio.h> 
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 
