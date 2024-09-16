548cfcf76f2a099ca5f19f4d8ddaa4832c7797ab
Jean-Pascal Richir <jrichir@student.s19.be>
2024-09-16 11:37:51 +0200

    Loop prompt

--- src/minishell.c
+++ src/minishell.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 11:11:01 by jrichir           #+#    #+#             */
-/*   Updated: 2024/09/13 16:19:25 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/09/16 11:36:36 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ int	main(int argc, char **argv)
 	(void)argc;
 	(void)argv;
 	prompt = NULL;
-	printf("Hello world!\n");
-	prompt = readline("minishell>");
-	printf("User input was: %s\n", prompt);
+	printf("=== MiNiSHELL v0.1 ===\n\n");
+	while ((prompt = readline("minishell>")))
+		printf("User input was: %s\n", prompt);
 	return (0);
 }
