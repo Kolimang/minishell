5442ba7378296da44d2edc8819f797b62d8c098d
Jean-Pascal Richir <jrichir@student.s19.be>
2024-09-16 14:08:46 +0200

    cmd history: basic implementation

--- src/minishell.c
+++ src/minishell.c
@@ -3,14 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lboumahd <lboumahd@student.s19.be>         +#+  +:+       +#+        */
+/*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 11:11:01 by jrichir           #+#    #+#             */
-/*   Updated: 2024/09/16 12:08:08 by lboumahd         ###   ########.fr       */
+/*   Updated: 2024/09/16 14:00:56 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+// Checks if the cmd string is empty, if it is not then it is added to history.
+void	ft_add_cmd_to_history(char *cmd)
+{
+	if (cmd && *cmd)
+		add_history(cmd);
+}
 
 int	main(int argc, char **argv)
 {
@@ -25,11 +32,16 @@ int	main(int argc, char **argv)
 */
 	printf("\033[0;38;5;214m=== MiNiSHELL v0.1 ===\033[0m\n\n");
 	prompt = "\033[0;32mminishell>\033[0m";
+	using_history();
 	cmd = readline(prompt);
+	ft_add_cmd_to_history(cmd);
 	while (prompt)
 	{
 		printf("Last input: %s\n", cmd);
+		free(cmd);
 		cmd = readline(prompt);
+		ft_add_cmd_to_history(cmd);
 	}
+	free(cmd);
 	return (0);
 }
