08c04dfb69765fef171e51b208c1e89cd5beff9f
Jean-Pascal Richir <jrichir@student.s19.be>
2024-09-16 11:54:00 +0200

    add colors

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 11:11:01 by jrichir           #+#    #+#             */
-/*   Updated: 2024/09/16 11:40:40 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/09/16 11:52:07 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ int	main(int argc, char **argv)
 
 	(void)argc;
 	(void)argv;
-	printf("=== MiNiSHELL v0.1 ===\n\n");
+	printf("\033[0;38;5;214m=== MiNiSHELL v0.1 ===\033[0m\n\n");
 	prompt = NULL;
-	prompt = readline("minishell>");
+	prompt = readline("\033[0;32mminishell>\033[0m");
 	while (prompt)
 	{
 		printf("Last input: %s\n", prompt);
-		prompt = readline("minishell>");
+		prompt = readline("\033[0;32mminishell>\033[0m");
 	}
 	return (0);
 }
