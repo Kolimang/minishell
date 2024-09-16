1a4442f4fb3349e6d28f2b51a3fca599db1e3663
lboumahdi <linaboumahdi1@gmail.com>
2024-09-16 14:47:04 +0200

    history

--- src/minishell.c
+++ src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
+/*   By: lboumahd <lboumahd@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 11:11:01 by jrichir           #+#    #+#             */
-/*   Updated: 2024/09/16 14:00:56 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/09/16 14:46:22 by lboumahd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,16 +26,11 @@ int	main(int argc, char **argv)
 
 	(void)argc;
 	(void)argv;
-/*
-	if(mini_loop () == -1)
-		return failure/exit;
-*/
-	printf("\033[0;38;5;214m=== MiNiSHELL v0.1 ===\033[0m\n\n");
+	printf("\033[0;38;5;214m=== MiNishell v0.1 ===\033[0m\n\n");
 	prompt = "\033[0;32mminishell>\033[0m";
-	using_history();
 	cmd = readline(prompt);
 	ft_add_cmd_to_history(cmd);
-	while (prompt)
+	while (1)
 	{
 		printf("Last input: %s\n", cmd);
 		free(cmd);
