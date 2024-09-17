45e88f18ff00ed8aca7e10148cd79a0bf9741b3d
Jean-Pascal Richir <jrichir@student.s19.be>
2024-09-17 11:37:08 +0200

    move main() content to execute()

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 11:11:01 by jrichir           #+#    #+#             */
-/*   Updated: 2024/09/17 11:04:13 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/09/17 11:34:09 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,11 @@ void	ft_add_cmd_to_history(char *cmd)
 		add_history(cmd);
 }
 
-int	main(int argc, char **argv)
+int	execute(void)
 {
 	char	*prompt;
 	char	*cmd;
-	int		res;
 
-	(void)argc;
-	(void)argv;
 	printf("\033[0;38;5;214m=== MiNishell v0.1 ===\033[0m\n\n");
 	prompt = "\033[0;32mminishell>\033[0m";
 	while (1)
@@ -36,6 +33,14 @@ int	main(int argc, char **argv)
 		ft_add_cmd_to_history(cmd);
 		free(cmd);
 	}
-	free(cmd);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	if (execute())
+		return (execute());
 	return (0);
 }
