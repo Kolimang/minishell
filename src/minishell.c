c5734186651fa4aae06cb97e1b75afed9dc398c7
Jean-Pascal Richir <jrichir@student.s19.be>
2024-10-02 17:12:52 +0200

    split command line at pipes and redirections

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 11:11:01 by jrichir           #+#    #+#             */
-/*   Updated: 2024/10/01 16:41:02 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/10/02 16:24:17 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,31 +55,55 @@ typedef struct s_lexems
 
 typedef struct s_cmd_data
 {
-	int	flag_in_sq;
-	int	flag_in_dq;
-	int	flag_endq_sep;
-	int	flag_endstr;
-	int	flag_delimit_token;
-	int	token_in_progress;
-	int	token_id;
+	int				bool_in_sq;
+	int				bool_in_dq;
+	int				bool_endq_sep;
+	int				bool_endstr;
+	int				bool_delimit_tok;
+	int				bool_tok_in_progress;
+	int				tok_id;
+	int				tok_len;
+	unsigned int	tok_start;
 }	t_cmd_data;
 
 void	init_cmd_data(t_cmd_data *data)
 {
-	data->flag_in_sq = 0;
-	data->flag_in_dq = 0;
-	data->flag_endq_sep = 0;
-	data->flag_endstr = 0;
-	data->flag_delimit_token = 0;
-	data->token_in_progress = 0;
-	data->token_id = 0;
+	data->bool_in_sq = 0;
+	data->bool_in_dq = 0;
+	data->bool_endq_sep = 0;
+	data->bool_endstr = 0;
+	data->bool_delimit_tok = 0;
+	data->bool_tok_in_progress = 0;
+	data->tok_id = 0;
+	data->tok_len = 0;
+	data->tok_start = 0;
+}
+
+static int	char_in_set(char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static int	is_operator(char c)
+{
+	char	*set;
+
+	set = "<|>";
+	return (char_in_set(set, c));
 }
 
 t_list	*ft_tokenize(char *cmd)
 {
 	int					i;
-	int					len;
-	unsigned int		tokenstart;
 	t_cmd_data			data;
 	char				*lexem;
 	t_list				*list_lexems;
@@ -88,97 +112,132 @@ t_list	*ft_tokenize(char *cmd)
 	if (ft_strlen(cmd) <= 0)
 		return (NULL);
 	init_cmd_data(&data);
-	tokenstart = 0;
-	len = 0;
 	i = 0;
 	list_lexems = NULL;
 	while (cmd[i])
 	{
-		if (cmd[i] == ' ')
+		// handle_operators();
+		// handle_spaces();
+		// handle_quotes();
+		// create_node();
+		if (is_operator(cmd[i]))
 		{
-			if (data.token_in_progress == 1 && data.flag_in_sq == 0 && data.flag_in_dq == 0)
-				data.flag_delimit_token = 1;
-			else if (data.token_in_progress == 0 && data.flag_in_sq == 0 && data.flag_in_dq == 0)
-				tokenstart += 1;
-			else if (data.flag_in_sq == 1 || data.flag_in_dq == 1)
-				data.token_in_progress = 1;
+			if (data.bool_in_sq == 0 && data.bool_in_dq == 0)
+			{
+				if (i > 0 && !is_operator(cmd[i - 1]))
+					data.bool_delimit_tok = 1;
+				else if (i > 0 && is_operator(cmd[i - 1]) && cmd[i - 1] != cmd[i])
+					data.bool_delimit_tok = 1;
+				//if (cmd[i + 1] && is_operator(cmd[i + 1]))
+				//	data.tok_len += 1;
+			}
+			else if (data.bool_in_sq == 1 || data.bool_in_dq == 1)
+				data.bool_tok_in_progress = 1;
+		}
+		else if (i > 0 && is_operator(cmd[i - 1]))
+			data.bool_delimit_tok = 1;
+		else if (cmd[i] == ' ')
+		{
+			if (data.bool_tok_in_progress == 1 && data.bool_in_sq == 0 && data.bool_in_dq == 0)
+					data.bool_delimit_tok = 1;
+			else if (data.bool_tok_in_progress == 0 && data.bool_in_sq == 0 && data.bool_in_dq == 0)
+				data.tok_start += 1;
+			else if (data.bool_in_sq == 1 || data.bool_in_dq == 1)
+				data.bool_tok_in_progress = 1;
 		}
 		else if (cmd[i] == '\'')
 		{
-			if (data.flag_in_dq == 0 && data.flag_in_sq == 1)
+			if (data.bool_in_dq == 0 && data.bool_in_sq == 1)
 			{
-				data.flag_in_sq = 0;
-				data.flag_delimit_token = 1;
-				data.flag_endq_sep = 1;
+				data.bool_in_sq = 0;
+				data.bool_delimit_tok = 1;
+				data.bool_endq_sep = 1;
 			}
-			else if (data.flag_in_dq == 0 && data.flag_in_sq == 0)
+			else if (data.bool_in_dq == 0 && data.bool_in_sq == 0)
 			{
-				data.flag_in_sq = 1;
-				if (data.token_in_progress == 1)
-					data.flag_delimit_token = 1;
+				data.bool_in_sq = 1;
+				if (data.bool_tok_in_progress == 1)
+					data.bool_delimit_tok = 1;
 				else
 				{
-					data.token_in_progress = 1;
+					data.bool_tok_in_progress = 1;
 				}
 			}
 		}
 		else if (cmd[i] == '\"')
 		{
-			if (data.flag_in_sq == 0 && data.flag_in_dq == 1)
+			if (data.bool_in_sq == 0 && data.bool_in_dq == 1)
 			{
-				data.flag_in_dq = 0;
-				data.flag_delimit_token = 1;
-				data.flag_endq_sep = 1;
+				data.bool_in_dq = 0;
+				data.bool_delimit_tok = 1;
+				data.bool_endq_sep = 1;
 			}
-			else if (data.flag_in_sq == 0 && data.flag_in_dq == 0)
+			else if (data.bool_in_sq == 0 && data.bool_in_dq == 0)
 			{
-				data.flag_in_dq = 1;
-				if (data.token_in_progress == 1)
-					data.flag_delimit_token = 1;
+				data.bool_in_dq = 1;
+				if (data.bool_tok_in_progress == 1)
+					data.bool_delimit_tok = 1;
 				else
-					data.token_in_progress = 1;
+					data.bool_tok_in_progress = 1;
 			}
 		}
 		else
-			data.token_in_progress = 1;
+			data.bool_tok_in_progress = 1;
 		if (cmd[i + 1] == '\0')
 		{
-			data.flag_endstr = 1;
-			data.flag_delimit_token = 1;
-			if (data.flag_in_sq == 1 || data.flag_in_dq == 1)
+			data.bool_endstr = 1;
+			data.bool_delimit_tok = 1;
+			if (data.bool_in_sq == 1 || data.bool_in_dq == 1)
 				printf("ERROR: unclosed quote.\n");
 		}
-		if (data.flag_delimit_token == 1)
+		if (data.bool_delimit_tok == 1)
 		{
-			if (&cmd[i] == &cmd[tokenstart])
-				len = 1;
+			if (&cmd[i] == &cmd[data.tok_start])
+				data.tok_len = 1;
 			else
-				len = &cmd[i] - &cmd[tokenstart];
-			if (data.flag_endq_sep == 1)
+				data.tok_len = &cmd[i] - &cmd[data.tok_start];
+			if (data.bool_endq_sep == 1)
 			{
-				data.flag_endq_sep = 0;
-				len += 1;
+				data.bool_endq_sep = 0;
+				data.tok_len += 1;
 			}
-			else if (data.flag_endstr == 1)
-				len += 1;
-			templexem = ft_substr(cmd, tokenstart, (size_t)(len));
+			else if (data.bool_endstr == 1)
+				data.tok_len += 1;
+			templexem = ft_substr(cmd, data.tok_start, (size_t)data.tok_len);
 			// To do: protect outputs from ft_substr and ft_strtrim (mallocs)
 			lexem = ft_strtrim(templexem, " ");
 			// To do: protect outputs from ft_substr and ft_strtrim (mallocs)
 			free(templexem);
-			printf("[%d] %s\n", data.token_id, lexem);
-			if (!list_lexems)
-				list_lexems = ft_lstnew(lexem);
+			if (lexem[0] != '\0')
+			{
+				if (!list_lexems)
+					list_lexems = ft_lstnew(lexem);
+				else
+					ft_lstadd_back(&list_lexems, ft_lstnew(lexem));
+			}
 			else
-				ft_lstadd_back(&list_lexems, ft_lstnew(lexem));
-			tokenstart += len;
-			data.token_id++;
-			data.token_in_progress = 0;
-			data.flag_delimit_token = 0;
+				free(lexem);
+			data.tok_start += data.tok_len;
+			data.tok_id++;
+			data.bool_tok_in_progress = 0;
+			data.bool_delimit_tok = 0;
 		}
 		i++;
 	}
 	return (list_lexems);
+}
+
+void	ft_print_list(t_list *list, char *title)
+{
+	if (!list)
+		return ;
+	if (title)
+		printf("%s\n", title);
+	while (list)
+	{
+		printf("%s\n", list->content);
+		list = list->next;
+	}
 }
 
 int	execute(void)
@@ -200,6 +259,7 @@ int	execute(void)
 			free(cmd);
 			return (1);
 		}
+		ft_print_list(lexems, "--- List of lexems ---");
 		ft_add_cmd_to_history(cmd);
 		free(cmd);
 	}
