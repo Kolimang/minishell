ef5686f35f1be3ca997110e8d9d723c60ccafb5b
Jean-Pascal Richir <jrichir@student.s19.be>
2024-10-08 15:25:25 +0200

    split tokenize() func into three funcs

--- src/lexer/tokenize.c
+++ src/lexer/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:16:04 by jrichir           #+#    #+#             */
-/*   Updated: 2024/10/08 12:26:22 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/10/08 15:04:15 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,59 +30,80 @@ t_list	*ft_tokenize(char *cmd)
 {
 	int			i;
 	t_cmd_data	data;
-	t_list		*list_lexems;
+	t_list		*list_lexemes;
 
 	if (ft_strlen(cmd) <= 0)
 		return (NULL);
 	init_cmd_data(&data);
 	i = 0;
-	list_lexems = NULL;
+	list_lexemes = NULL;
 	while (cmd[i])
 	{
 		lex_handle_operators(cmd, i, &data);
 		lex_handle_spaces(cmd, i, &data);
 		lex_handle_quotes(cmd, i, &data);
-		create_node(cmd, i, &data, &list_lexems);
+		lex_handle_regular(cmd, i, &data);
+		lex_handle_end_of_cmd(cmd, i, &data);
+		create_node(cmd, i, &data, &list_lexemes);
 		i++;
 	}
-	ft_print_list(lex_handle_heredoc(&data, find_delim(list_lexems)), "hd:\n");
-	return (list_lexems);
+	ft_print_list(lex_handle_heredoc(&data, find_delim(list_lexemes)), "hd:\n");
+	return (list_lexemes);
 }
 
 // first if-part inside the main "IF" can possibly be removed, must be tested
-void	create_node(char *cmd, int i, t_cmd_data *data, t_list	**list_lexems)
+void	create_node(char *cmd, int i, t_cmd_data *data, t_list	**list_lexemes)
 {
-	char	*templexem;
-	char	*lexem;
+	char		*templexeme_str;
+	char		*lexeme_str;
+	t_lexemes	*lexeme;
 
 	if (data->bool_delimit_tok == 1)
 	{
-		if (&cmd[i] == &cmd[data->tok_start])
-			data->tok_len = 1;
-		else
-			data->tok_len = &cmd[i] - &cmd[data->tok_start];
-		if (data->bool_endq_sep == 1)
+		set_token_len(cmd, i, data);
+		templexeme_str = ft_substr(cmd, data->tok_start, (size_t)data->tok_len);
+		lexeme_str = ft_strtrim(templexeme_str, " ");
+		free(templexeme_str);
+		if (lexeme_str[0] != '\0')
 		{
-			data->bool_endq_sep = 0;
-			data->tok_len += 1;
-		}
-		else if (data->bool_endstr == 1)
-			data->tok_len += 1;
-		templexem = ft_substr(cmd, data->tok_start, (size_t)data->tok_len);
-		lexem = ft_strtrim(templexem, " ");
-		free(templexem);
-		if (lexem[0] != '\0')
-		{
-			if (!*list_lexems)
-				*list_lexems = ft_lstnew(lexem);
+			lexeme = malloc(sizeof(t_lexemes));
+			lexeme->index = data->tok_id;
+			lexeme->str = lexeme_str;
+			lexeme->value = NULL;
+			if (!*list_lexemes)
+				*list_lexemes = ft_lstnew(lexeme);
 			else
-				ft_lstadd_back(list_lexems, ft_lstnew(lexem));
+				ft_lstadd_back(list_lexemes, ft_lstnew(lexeme));
 		}
 		else
-			free(lexem);
-		data->tok_start += data->tok_len;
-		data->tok_id++;
-		data->bool_tok_in_progress = 0;
-		data->bool_delimit_tok = 0;
+			free(lexeme_str);
+		reset_token_data(data);
 	}
+}
+
+void	reset_token_data(t_cmd_data *data)
+{
+	if (!data)
+		return ;
+	data->tok_start += data->tok_len;
+	data->tok_id++;
+	data->bool_tok_in_progress = 0;
+	data->bool_delimit_tok = 0;
+}
+
+void	set_token_len(char *cmd, int i, t_cmd_data *data)
+{
+	if (!cmd || !data)
+		return ;
+	if (&cmd[i] == &cmd[data->tok_start])
+		data->tok_len = 1;
+	else
+		data->tok_len = &cmd[i] - &cmd[data->tok_start];
+	if (data->bool_endq_sep == 1)
+	{
+		data->bool_endq_sep = 0;
+		data->tok_len += 1;
+	}
+	else if (data->bool_endstr == 1)
+		data->tok_len += 1;
 }
