40675ed464f988185da6d1a37fb5fd682056cc34
lboumahdi <linaboumahdi1@gmail.com>
2024-10-09 17:23:57 +0200

    again

--- include/expander.h
+++ include/expander.h
@@ -6,7 +6,7 @@
 /*   By: lboumahd <lboumahd@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:08:51 by jrichir           #+#    #+#             */
-/*   Updated: 2024/10/09 16:15:56 by lboumahd         ###   ########.fr       */
+/*   Updated: 2024/10/09 17:10:55 by lboumahd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,8 @@ char	*handle_exp(char *tmp, t_lexemes *lexeme, t_env *new_env);
 void	process_regular(t_lexemes *lexeme, t_env *new_env);
 void	expand_lexeme(t_lexemes *lexeme, t_env *new_env);
 void	expander(char **res, char *tmp, int *i, t_env *new_env);
-//char	*find_var(char *var, t_env *new_env);
-//char	*replace_var(char *tmp, int *i, int start, t_env *new_env);
 void	append_to_str(char **res, char *tmp, int end, int start);
 void	handle_nq(char **res, char *tmp, int *i, t_env *new_env, t_lexemes *lexeme);
-//void	process_hrdoc(t_lexemes *lexeme);
 void	dup_word(char **res, char *str, int *i);
 //HR
 void process_hrdoc(t_lexemes *lexeme, t_env *new_env);
