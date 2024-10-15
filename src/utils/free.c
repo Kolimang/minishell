815f4dca720c1797e0d233558c5dc9e7b201ba2a
Jean-Pascal Richir <jrichir@student.s19.be>
2024-10-15 14:26:31 +0200

    add: array_str_free() function

--- src/utils/free.c
+++ src/utils/free.c
@@ -6,11 +6,25 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 12:44:04 by jrichir           #+#    #+#             */
-/*   Updated: 2024/10/15 13:47:17 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/10/15 14:19:20 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+void	array_str_free(char **array, int limit)
+{
+	int	i;
+
+	if (!array)
+		return ;
+	i = -1;
+	while (++i < limit)
+	{
+		free(array[i]);
+	}
+	free(array);
+}
 
 void	cleanup_lexemes(t_lexemes *lexeme)
 {
