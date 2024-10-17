807ee94de265a1c184a1f65fcbad6eb8aee27a0f
Jean-Pascal Richir <jrichir@student.s19.be>
2024-10-17 17:05:14 +0200

    fix: parentesis at wrong place in ft_echo()

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:08:58 by jrichir           #+#    #+#             */
-/*   Updated: 2024/10/17 16:57:16 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/10/17 17:04:51 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_echo(char **args)
 	newline = 1;
 	if (args && args[1])
 	{
-		if (ft_strncmp(args[1], "-n", 2 == NULL))
+		if (ft_strncmp(args[1], "-n", 2) == NULL)
 		{
 			i = 2;
 			newline = 0;
