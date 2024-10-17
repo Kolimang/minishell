8ce780429326f2fb70c46c7682f8462627c099be
Jean-Pascal Richir <jrichir@student.s19.be>
2024-10-17 14:19:03 +0200

    removed comment

--- src/lexer/handle_symbols1.c
+++ src/lexer/handle_symbols1.c
@@ -6,7 +6,7 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:16:04 by jrichir           #+#    #+#             */
-/*   Updated: 2024/10/10 16:31:49 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/10/17 13:25:01 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,9 @@ void	lex_handle_operators(char *cmd, int i, t_cmd_data *data)
 					data->bool_heredoc = 1;
 			}
 		}
-		//else if (data->bool_in_sq == 1 || data->bool_in_dq == 1) // commented out for test
-		//	data->bool_tok_in_progress = 1; // commented out for test
+		// Thw two lines below commented out for test
+		// else if (data->bool_in_sq == 1 || data->bool_in_dq == 1)
+		//	data->bool_tok_in_progress = 1;
 		data->bool_tok_in_progress = 1;// added for test
 	}
 	else if (i > 0 && is_operator(cmd[i - 1]))
@@ -57,7 +58,7 @@ void	lex_handle_spaces(char *cmd, int i, t_cmd_data *data)
 		else if (data->bool_tok_in_progress == 0
 			&& data->bool_in_sq == 0
 			&& data->bool_in_dq == 0
-			&& cmd[i + 1] != '\0') // added for test : <f_ ok mais <f__ plante ('_' etant un espace, pas un underscore)
+			&& cmd[i + 1] != '\0')
 			data->tok_start += 1;
 		else if (data->bool_in_sq == 1 || data->bool_in_dq == 1)
 			data->bool_tok_in_progress = 1;
