de6e34d56f60aafc4c855e6ac2fad8c8bf1272e4
Jean-Pascal Richir <jrichir@student.s19.be>
2024-10-17 14:14:00 +0200

    norm: space-->tab

--- include/expander.h
+++ include/expander.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expander.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lboumahd <lboumahd@student.s19.be>         +#+  +:+       +#+        */
+/*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:08:51 by jrichir           #+#    #+#             */
-/*   Updated: 2024/10/09 17:10:55 by lboumahd         ###   ########.fr       */
+/*   Updated: 2024/10/17 13:50:58 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,14 @@ void	ft_expand_lexeme_list(t_list *list, t_env *env);
 // Lina 
 void	handle_sq(char **res, char *tmp, int *i, int start);
 void	handle_dq(char **res, char *tmp, int *i, t_env *new_env);
-char	*handle_exp(char *tmp, t_lexemes *lexeme, t_env *new_env);
-void	process_regular(t_lexemes *lexeme, t_env *new_env);
-void	expand_lexeme(t_lexemes *lexeme, t_env *new_env);
+char	*handle_exp(char *tmp, t_lexeme *lex, t_env *new_env);
+void	process_regular(t_lexeme *lex, t_env *new_env);
+void	expand_lexeme(t_lexeme *lex, t_env *new_env);
 void	expander(char **res, char *tmp, int *i, t_env *new_env);
 void	append_to_str(char **res, char *tmp, int end, int start);
-void	handle_nq(char **res, char *tmp, int *i, t_env *new_env, t_lexemes *lexeme);
+void	handle_nq(char **res, char *tmp, int *i, t_env *new_env,
+			t_lexeme *lex);
 void	dup_word(char **res, char *str, int *i);
 //HR
-void process_hrdoc(t_lexemes *lexeme, t_env *new_env);
+void	process_hrdoc(t_lexeme *lex, t_env *new_env);
 #endif
