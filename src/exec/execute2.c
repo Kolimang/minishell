5564471e4a28d4f0a601c3cfe5baf043e437144f
Jean-Pascal Richir <jrichir@student.s19.be>
2024-10-17 13:56:50 +0200

    norm: add space after keywords

--- src/exec/execute2.c
+++ src/exec/execute2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute2.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lboumahd <lboumahd@student.s19.be>         +#+  +:+       +#+        */
+/*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 13:21:00 by lboumahd          #+#    #+#             */
-/*   Updated: 2024/10/16 17:03:08 by lboumahd         ###   ########.fr       */
+/*   Updated: 2024/10/17 13:12:35 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ void	execute_redir(t_command *cmd, t_io_fd *files)
 {
 	t_redir	*redir;
 
-	while(cmd->ls_redirs)
+	while (cmd->ls_redirs)
 	{
 		redir = cmd->ls_redirs->content;
-		if(redir->type == INFILE || redir->type == HERE_DOC)
+		if (redir->type == INFILE || redir->type == HERE_DOC)
 			get_infile();
-		if(redir->type == OUTFILE || redir->type == APPEND)
-			// ...
+		if (redir->type == OUTFILE || redir->type == APPEND)
+			;// ...
 		cmd->ls_redirs = cmd->ls_redirs->next;
 	}
 }
