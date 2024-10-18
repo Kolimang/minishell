18ea2eafca811a739d2fe3509de83d89ccc4eade
lboumahdi <linaboumahdi1@gmail.com>
2024-10-18 13:08:43 +0200

    WIP

--- include/exec.h
+++ include/exec.h
@@ -6,7 +6,7 @@
 /*   By: lboumahd <lboumahd@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 18:57:26 by lboumahd          #+#    #+#             */
-/*   Updated: 2024/10/17 17:34:06 by lboumahd         ###   ########.fr       */
+/*   Updated: 2024/10/18 13:08:17 by lboumahd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int execute_redir(t_command *cmd, t_io_fd *io);
 // execute_nofork(t_command *cmd, t_env *local_env, char **global_env);
 
 void	child_heredoc_process(t_command *cmd, t_env *local_env, int fd[2]);
-void parent_heredoc_process(t_command *cmd, pid_t pid, int pipe_fd[2]);
+int	parent_heredoc_process(t_command *cmd, pid_t pid, int pipe_fd[2]);
 void	reset_io(t_command *cmd);
 int	get_outfile(t_command *cmd, char *name, t_io_fd *files, int flag);
 int	get_infile(t_command *cmd, char *name, t_io_fd *files, int flag);
