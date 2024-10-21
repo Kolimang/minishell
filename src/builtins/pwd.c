db52dbe52b94a5ddee164ba61f96165418227ec0
Jean-Pascal Richir <jrichir@student.s19.be>
2024-10-21 13:59:58 +0200

    handle when env var PWD is unset

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,27 +6,37 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:08:58 by jrichir           #+#    #+#             */
-/*   Updated: 2024/10/18 13:12:18 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/10/21 13:29:30 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-// pwd does not accept arguments
 int	ft_pwd(char **args, t_env *env)
 {
+	char	*path;
+
 	if (args && args[1])
-		ft_putstr_fd("pwd: too many arguments", 2);
-	if (!env)
-		return (-1);
-	while (env)
+		return (ft_putstr_fd("pwd: too many arguments\n", 2), 1);
+	if (env)
 	{
-		if (ft_strncmp(env->var_name, "PWD", 3) == 0)
+		while (env)
 		{
-			ft_printf("%s\n", env->var_val);
-			return (0);
+			if (ft_strncmp(env->var_name, "PWD", 3) == 0)
+			{
+				ft_printf("%s\n", env->var_val);
+				return (0);
+			}
+			env = env->next;
 		}
-		env = env->next;
 	}
-	return (-1);
+	path = getcwd(NULL, 0);
+	if (path)
+	{
+		ft_printf("%s\n", path);
+		free(path);
+	}
+	else
+		return (perror(path), 1);
+	return (0);
 }
