8704db9d9166ad8e3aa9e8069e12d1e9fb09d654
Jean-Pascal Richir <jrichir@student.s19.be>
2024-10-27 19:33:54 +0100

    emptied ft_exit() body

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:08:58 by jrichir           #+#    #+#             */
-/*   Updated: 2024/10/27 19:06:24 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/10/27 19:32:48 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,6 @@
 
 int	ft_exit(char **args, t_env *env)
 {
-	char	*name;
-	char	*value;
-	int		i;
-	int		res;
-
-	res = 0;
-	if (args && !args[1])
-		return (print_env(env, 1), 0);
-	if (args && args[1])
-	{
-		i = 1;
-		while (args[i])
-		{
-			res = check_name(args[i]);
-			if (res)
-				merror(args[0], args[i], "not a valid identifier", 1);
-			else
-			{
-				check_value(args[i], &name, &value);
-				update_env(name, value, &env);
-			}
-			i++;
-		}
-	}
-	return (res);
+	//
+	return (0);
 }
