9abecf2af5b896f3c45bf1bde48a97c22ad48479
Jean-Pascal Richir <jrichir@student.s19.be>
2024-10-28 17:15:28 +0100

    builtin: add ft_exit()

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:08:58 by jrichir           #+#    #+#             */
-/*   Updated: 2024/10/28 13:05:12 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/10/28 16:42:22 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 
 int	ft_exit(char **args, t_env *env)
 {
-	(void)args; // temp
-	(void)env; // temp, to be freed
-	// free everything ?
+	array_str_free(args, ft_arraylen(args));
+	free_env(env);
 	ft_printf("exit\n");
 	exit(g_ret_value);
 }
