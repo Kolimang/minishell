fcc65b0ae2c1c740bfb858d64b9bf24540d3c8ac
Jean-Pascal Richir <jrichir@student.s19.be>
2024-10-28 17:15:10 +0100

    builtin: add unset()

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,20 +6,67 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:08:58 by jrichir           #+#    #+#             */
-/*   Updated: 2024/10/27 19:32:52 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/10/28 16:45:26 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-int	ft_unset(char **args, t_env *env)
+static int	unset_head(t_env **env)
 {
-	char	*name;
-	char	*value;
-	int		i;
-	int		res;
+	t_env	*temp;
 
-	res = 0;
+	temp = *env;
+	if ((*env)->next)
+		*env = (*env)->next;
+	free(temp->var_name);
+	temp->var_name = NULL;
+	free(temp->var_val);
+	temp->var_val = NULL;
+	free(temp);
+	temp = NULL;
+	return (0);
+}
+
+static int	unset_node(t_env *node, t_env *prev)
+{
+	prev->next = node->next;
+	free(node->var_name);
+	node->var_name = NULL;
+	free(node->var_val);
+	node->var_val = NULL;
+	free(node);
+	node = NULL;
+	return (0);
+}
+
+static int	handle_unset(t_env **env, char *var_name)
+{
+	t_env	*prev;
+	t_env	*curr;
+
+	curr = *env;
+	if (!ft_strncmp(curr->var_name, var_name, ft_strlen(var_name) + 1))
+	{
+		unset_head(env);
+		return (0);
+	}
+	prev = *env;
+	curr = curr->next;
+	while (curr)
+	{
+		if (!ft_strncmp(curr->var_name, var_name, ft_strlen(var_name) + 1))
+			unset_node(curr, prev);
+		curr = curr->next;
+		prev = prev->next;
+	}
+	return (0);
+}
+
+int	ft_unset(char **args, t_env **env)
+{
+	int	i;
+
 	if (args && !args[1])
 		return (merror(args[0], NULL, "not enough arguments", 1));
 	if (args && args[1])
@@ -27,12 +74,11 @@ int	ft_unset(char **args, t_env *env)
 		i = 1;
 		while (args[i])
 		{
-			while (env)
-			{
-				;//
-			}
+			handle_unset(env, args[i]);
+			ft_printf("%p\n", *env);//---- DEBUG
+			ft_printf("%s\n", (*env)->var_name);//---- DEBUG
 			i++;
 		}
 	}
-	return (res);
+	return (0);
 }
