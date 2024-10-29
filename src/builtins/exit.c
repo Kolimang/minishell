47c500266400adc58c1d307446ff3f8b89223fa2
Jean-Pascal Richir <jrichir@student.s19.be>
2024-10-29 12:58:37 +0100

    ft_exit() better mimics bash exit()

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:08:58 by jrichir           #+#    #+#             */
-/*   Updated: 2024/10/29 12:49:33 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/10/29 12:57:56 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,29 +20,30 @@ int	arg_is_number(char *arg)
 	while (arg[i])
 	{
 		if (!ft_isdigit(arg[i]))
-			return (EXIT_FAILURE);
+			return (0);
 		i++;
 	}
-	return (EXIT_SUCCESS);
+	return (1);
 }
 
 int	ft_exit(char **args, t_env *env)
 {
 	int	argc;
 
+	ft_printf("exit\n");
 	argc = ft_arraylen(args);
-	if (argc > 2)
-	{
-		g_ret_value = 1;
-		merror(args[0], NULL, "too many arguments\n", 1);
-	}
-	else if (args[1] && !arg_is_number(args[1]))
+	if (args[1] && !arg_is_number(args[1]))
 	{
 		g_ret_value = 255;
-		merror(args[0], args[1], "numeric argument required\n", 1);
+		merror(args[0], args[1], "numeric argument required", 1);
+	}
+	else if (argc > 2)
+	{
+		g_ret_value = 1;
+		merror(args[0], NULL, "too many arguments", 1);
 	}
 	array_str_free(args, ft_arraylen(args));
 	free_env(env);
-	ft_printf("exit\n");
+	// free command(s) ?
 	exit(g_ret_value);
 }
