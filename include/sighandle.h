4b5e6da62df4cca0a7dada4d31d2df401935e622
Jean-Pascal Richir <jrichir@student.s19.be>
2024-10-29 14:27:45 +0100

    fix sighandle header

--- include/sighandle.h
+++ include/sighandle.h
@@ -6,14 +6,14 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 14:08:51 by jrichir           #+#    #+#             */
-/*   Updated: 2024/10/29 14:06:52 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/10/29 14:27:13 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUILTIN_H
-# define BUILTIN_H
+#ifndef SIGHANDLE_H
+# define SIGHANDLE_H
 
 void	init_signals(void);
-int		ft_sighandler(int signum);
+void	ft_sighandler(int signum);
 
 #endif
