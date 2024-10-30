bfd2b922be49e4db6473ad6181871b8abfc49028
Jean-Pascal Richir <jrichir@student.s19.be>
2024-10-30 09:32:55 +0100

    feat:signal (still WiP)

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,21 +6,22 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:03:41 by jrichir           #+#    #+#             */
-/*   Updated: 2024/10/29 14:12:27 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/10/29 17:06:49 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+// SIGINT  (Ctrl-C) -> Display a new prompt
+// EOF     (Ctrl-D) -> Exit the shell
+// SIGQUIT (Ctrl-\) -> Ignore
 void	ft_sighandler(int signum)
 {
 	if (signum == SIGINT)
 	{
+		ft_printf("\n");
+		// terminate child processes
 		main(0, NULL, NULL);
-	}
-	else if (signum == EOF)
-	{
-		ft_exit(NULL, NULL); // leak of args and env
 	}
 	else if (signum == SIGQUIT)
 	{
@@ -29,8 +30,8 @@ void	ft_sighandler(int signum)
 }
 
 // SIGINT  (Ctrl-C) -> Display a new prompt
-// EOF     (Ctrl-D) -> Exit the shell
 // SIGQUIT (Ctrl-\) -> Ignore
+// EOF     (Ctrl-D) -> Exit the shell, if done at the beginning of the line (not a signal per se)
 void	init_signals(void)
 {
 	sigset_t			signal_set;
