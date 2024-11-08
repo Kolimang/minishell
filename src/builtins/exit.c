515fa989b2e21241ae0c62d8609f02641f1a6a86
kolimang <jprichir@hotmail.com>
2024-11-08 13:11:17 +0100

    ft_exit() : if numeric argument passed, returns arg % 256

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:08:58 by jrichir           #+#    #+#             */
-/*   Updated: 2024/11/06 11:07:41 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/11/08 13:09:38 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,21 @@ static int	arg_is_number(char *arg)
 int	ft_exit(char **args, t_env *env, int eof)
 {
 	int	argc;
+	int	ret;
 
 	if (eof)
 		ft_printf("\033[A\033[11C");
 	ft_printf("exit\n");
+	ret = 0;
 	argc = ft_arraylen(args);
 	if (args && args[1] && !arg_is_number(args[1]))
 	{
 		g_ret_value = 255;
 		merror(args[0], args[1], "numeric argument required", 1);
+	}
+	if (args && args[1] && arg_is_number(args[1]))
+	{
+		g_ret_value = ft_atoi(args[1]) % 256;
 	}
 	else if (argc > 2)
 	{
@@ -45,7 +51,6 @@ int	ft_exit(char **args, t_env *env, int eof)
 		merror(args[0], NULL, "too many arguments", 1);
 	}
 	array_str_free(args, ft_arraylen(args));
-	free_env(env);
-	// free command(s) ?
+	free_env(&env);
 	exit(g_ret_value); // Check if I should really return that value
 }
