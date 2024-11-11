02eee9b6d82254aa8b2af21405927797a0016e29
kolimang <jprichir@hotmail.com>
2024-11-11 13:53:57 +0100

    fix: do not print env if PATH is unset

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: jrichir <jrichir@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:08:58 by jrichir           #+#    #+#             */
-/*   Updated: 2024/11/06 11:06:24 by jrichir          ###   ########.fr       */
+/*   Updated: 2024/11/11 13:35:24 by jrichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,23 @@
 
 int	ft_env(char **args, t_env *env)
 {
+	t_env	*temp;
+	int		print;
+
 	if (!env)
 		return (merror(args[0], NULL, "no environment set", 1));
 	if (args && args[1])
 		return (merror(args[0], NULL, "too many arguments", 1));
+	print = 0;
+	temp = env;
+	while (temp)
+	{
+		if (!ft_strncmp(temp->var_name, "PATH", 5) && temp->var_val)
+			print = 1;
+		temp = temp->next;
+	}
+	if (print == 0)
+		return (merror(args[0], NULL, NULL, 1271));
 	if (args && !args[1])
 		print_env(env, 2);
 	return (set_exit_status(0));
