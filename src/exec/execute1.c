8541335e7acc2fdcaedd52565f3e29adc2eb7c18
jrichir <jrichir@student.s19.be>
2024-11-14 21:57:24 +0100

    working version

--- src/exec/execute1.c
+++ src/exec/execute1.c
@@ -12,20 +12,20 @@
 
 #include <minishell.h>
 
-int	execute_nofork(t_command *cmd, t_io_fd *io, t_env **l_env, char **g_env)
+int	execute_nofork(t_command *cmd, t_io_fd *io, t_env **l_env, t_list *cmds)
 {
 	int	ret_value;
 
 	if (set_fds(cmd, io) == -1)
 		return (-1);
-	ret_value = exec_builtin(cmd, l_env, g_env, 0);
+	ret_value = exec_builtin(cmd, l_env, cmds, 0);
 	if (cmd->fd_hrdoc != -3)
 		close(cmd->fd_hrdoc);
 	return (ret_value);
 }
 
 // res = -1; --> Error: unknown built-in command
-int	exec_builtin(t_command *cmd, t_env **l_env, char **g_env, int flag)
+int	exec_builtin(t_command *cmd, t_env **l_env, t_list *cmds, int flag)
 {
 	int	res;
 
